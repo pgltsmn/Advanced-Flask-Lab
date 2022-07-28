7303598b079631f7b0f4d8979426950f9dba1667
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2022-07-28 09:34:45 +0300

    mb not the last version but here

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -15,7 +15,7 @@ user_bio = "idk what sounds cats make so imma just write rrrr"
 posts = {
     "static/images/thumbs/confusedasfcat.jpg": "when you have 5 labs to do and 15 minutes to submit",
     "static/images/thumbs/joycat.jpeg": "me when instructors typing my code for me",
-    "static/images/thumbs/mecat.jpg": "instructors when students ask questions they could've find the answer to in google"}
+    "static/images/thumbs/mecat.jpg": "instructors when students ask questions they could've found the answer to in google"}
 
 
 #####
